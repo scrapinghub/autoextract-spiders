42e7e3cc9ce44db1f38e87187950ea1efba573ac
Viktor Shlapakov <vshlapakov@gmail.com>
2019-11-18 10:57:35 +0300

    Add HCF manager script

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     author='Scrapinghub Inc',
     description='Scrapinghub AutoExtract spiders',
     packages=find_packages(exclude=['tests']),
+    scripts=['scripts/manager.py'],
     entry_points={'scrapy': ['settings = autoextract_spiders.settings']},
 )
